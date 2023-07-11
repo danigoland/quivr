1d95db29808b92cb063f5b356768f5136948f9d6
Dani Goland <glossman@gmail.com>
2023-07-10 22:14:22 -0700

    Bug fixes due to resync with fork

--- backend/parsers/json.py
+++ backend/parsers/json.py
@@ -2,7 +2,8 @@ from fastapi import UploadFile
 from langchain.document_loaders import JSONLoader
 
 from .common import process_file
+from models.settings import CommonsDep
 
 
-def process_json(file: UploadFile, enable_summarization, user, user_openai_api_key):
-    return process_file(file, JSONLoader, "json", enable_summarization, user, user_openai_api_key)
+def process_json(commons: CommonsDep, file: UploadFile, enable_summarization, user, user_openai_api_key):
+    return process_file(commons, file, JSONLoader, enable_summarization, user, user_openai_api_key)
--- backend/parsers/python.py
+++ backend/parsers/python.py
@@ -2,7 +2,8 @@ from fastapi import UploadFile
 from langchain.document_loaders import PythonLoader
 
 from .common import process_file
+from models.settings import CommonsDep
 
 
-def process_py(file: UploadFile, enable_summarization, user, user_openai_api_key):
-    return process_file(file, PythonLoader, "py", enable_summarization, user, user_openai_api_key)
+def process_py(commons: CommonsDep, file: UploadFile, enable_summarization, user, user_openai_api_key):
+    return process_file(commons, file, PythonLoader, enable_summarization, user, user_openai_api_key)
